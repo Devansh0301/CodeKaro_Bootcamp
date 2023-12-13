1d9885f389005dfff674e6a211c652d1383548f6
Devansh0301 <130764624+Devansh0301@users.noreply.github.com>
2023-12-13 18:27:36 +0530

    Create Assignment1.js

---
+++ DAY!/Assignment1.js
@@ -0,0 +1,27 @@
+// ASSIGNMENT 1 : ARRAY OPERATIONS
+
+// 1. Create an empty array called "fruits".
+let fruits = []
+console.log(fruits)
+
+// 2. Add the following fruits to the "fruits" array: "apple", "banana", "orange".
+fruits.push('apple')
+fruits.push('banana')
+fruits.push('orange')
+console.log(fruits)
+
+// 3. Remove the first fruit from the array.
+fruits.shift()
+console.log(fruits)
+
+// 4. Add "grape" to the end of the array.
+fruits.push('grape')
+console.log(fruits)
+
+// 5. Update the second fruit in the array to "pear".
+fruits[1]='pear'
+console.log(fruits)
+
+// 6. Print the final "fruits" array after performing the above operations.
+console.log(fruits)
+
